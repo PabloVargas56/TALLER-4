ac9258ed6494ae33819a5280bf5f0212dba6d420
PabloVargas56 <rojaspablotomas@gmail.com>
2025-03-22 08:53:00 -0500

    Implementacion de la base de datos

--- TALLER-4.py
+++ TALLER-4.py
@@ -2,3 +2,16 @@ import sqlite3
 
 conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        price INTEGER,
+        cantidad INTEGER,
+        Categoria TEXT
+    )
+""")
+
+conn.commit()
+conn.close()
